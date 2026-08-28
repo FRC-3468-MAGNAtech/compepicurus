6152e82a8a357defdc404b3e50db65e61349ac82
yoruden <jormanthetam20102012@gmail.com>
2026-08-27 19:38:00 -0500

    main for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
   <div className="container mx-auto py-10">
-            <h1 className="text-3x1 font-bold">Welcome to My Next.js Site</h1>
-            <p className="mt-4">This is my first Next.js page using the App Router.</p>
+            <h1 className="text-3x1 font-bold">Website WIP</h1>
           </div>
   );
 }
